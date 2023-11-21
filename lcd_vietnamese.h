fe2b93de1763445dff0fe3c31dca51e8356bd40a
syntaxerrorer <134620459+syntaxerrorer@users.noreply.github.com>
2023-11-21 19:07:03 +0700

    Update lcd_vietnamese.h

--- lcd_vietnamese.h
+++ lcd_vietnamese.h
@@ -1,4 +1,4 @@
-byte small_a_with_breve[8] = { //[ă]
+byte small_a_with_breve[8] = {      
 	0b01010,
 	0b00100,
 	0b00000,
@@ -7,5 +7,37 @@ byte small_a_with_breve[8] = { //[ă]
 	0b01111,
 	0b00000,
 	0b00000
-};
+}; //ă
 
+byte small_a_with_circumflex[8] = {
+	0b00100,
+	0b01010,
+	0b00000,
+	0b01110,
+	0b10010,
+	0b01111,
+	0b00000,
+	0b00000
+};//â
+
+byte small_e_with_circumflex[8] = {
+	0b01110,
+	0b00001,
+	0b01100,
+	0b10010,
+	0b11100,
+	0b10000,
+	0b01100,
+	0b00001
+};//ê
+
+byte small_o_with_circumflex[8] = {
+	0b00100,
+	0b01010,
+	0b00000,
+	0b01110,
+	0b10001,
+	0b10001,
+	0b01110,
+	0b00000
+};//ô
